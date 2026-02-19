17dd992b2d74c334de9c4894dfd0acd16feb3d7e
drinksooya <yamguiang123@gmail.com>
2026-02-19 20:00:55 +0800

    "Added some changes to the origins"

--- backend/api/main.py
+++ backend/api/main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "https://drinksooya.github.io",
 ]
 
 production_url = os.getenv("RENDER_EXTERNAL_URL")
